f67bf3d365cbcbf2105e5284dbcda5ce02fd6362
Adán García <adgarboc@gmail.com>
2021-06-28 00:41:17 -0500

    Add feed

--- posts/views.py
+++ posts/views.py
@@ -4,31 +4,35 @@ from datetime import datetime
 
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yésica Cortés',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 
 # Create your views here.
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append(f'<p><strong>{post["name"]}</strong></p>'
-                       f'<p><small>{post["user"]} - <i>{post["timestamp"]}</i></small></p>'
-                       f'<figure><img src="{post["picture"]}"/></figure>')
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
